c226031d57bc05cdc3b3e055bbd0f03310e1af30
Nicolás Pascual González <nicolaspascualgonzalez@gmail.com>
2019-02-16 05:32:50 +0100

    Finished request

--- frontend/src/scripts/uploader.js
+++ frontend/src/scripts/uploader.js
@@ -24,64 +24,16 @@ export default class Uploader {
 	}
 
 	upload() {
-		//this.form.submit();
+		var formData = new FormData();
+		formData.append('file', this.file_input.files[0]);
 
-		let response = {
-			'msg_count': {
-				'jorge': 200,
-				'damian': 350,
-				'nicolas': 150,
-				'sergio': 50
-			},
-			'word_count': {
-				'jorge': 450,
-				'damian': 490,
-				'nicolas': 900,
-				'sergio': 300
-			},
-			'most_used_word': 'francisco',
-			'most_used_emoji': '🧠',
-			'emoji_count': {
-				'jorge': 30,
-				'damian': 90,
-				'nicolas': 0,
-				'sergio': 3
-			},
-			'msg_per_hour': {
-				'0': 15,
-				'1': 18,
-				'2': 35,
-				'3': 65,
-				'4': 5,
-				'6': 25,
-				'7': 105,
-				'8': 85,
-				'9': 46,
-				'10': 7,
-				'11': 15,
-				'12': 15,
-				'13': 15,
-				'14': 69,
-				'15': 67,
-				'16': 49,
-				'17': 27,
-				'18': 19,
-				'19': 8,
-				'20': 15,
-				'21': 90,
-				'22': 22,
-				'23': 100
-			},
-			'msg_per_week_day': {
-				'0': 190,
-				'1': 150,
-				'2': 190,
-				'3': 190,
-				'4': 156,
-				'6': 180
-			}
-		};
+		var request = new XMLHttpRequest();
+		request.responseType = 'json';
 
-		this.notify_all(response);
+		request.onload  = () => this.notify_all(request.response);
+
+		request.send(formData);
+
+		//this.notify_all(response);
 	}
 }
